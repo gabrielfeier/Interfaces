d9cc3dcc714491d788a260dc5e2720ee1e638c4b
Gabriel Feier <Gabriel Feier>
2016-08-12 00:29:14 -0300

    Continuação exercicios

---
+++ src/br/com/Interfaces/AreaCalculavel.java
@@ -0,0 +1,5 @@
+package br.com.Interfaces;
+
+public interface AreaCalculavel {
+	double calculaArea();
+}
---
+++ src/br/com/Interfaces/TesteClass.java
@@ -0,0 +1,8 @@
+package br.com.Interfaces;
+
+public class TesteClass {
+	public static void main(String[] args){
+		AreaCalculavel a = new Retangulo(3,2);
+		System.out.println(a.calculaArea());
+	}
+}
